a9389ece0ea46e4f0aa55879fc469479ef00f4ff
NVA <vieetanhnguyeenx@gmail.com>
2024-03-19 20:36:31 +0700

    .

---
+++ Group8-SuperMarketManagement/SuperMarketManagement/Controllers/AccountsController.cs
@@ -0,0 +1,40 @@
+﻿using DataAccess.DTOs;
+using DataAccess.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace SuperMarketManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IEmployeeRepository repository;
+        public AccountsController(IEmployeeRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpPost("SignUp")]
+        public async Task<IActionResult> SignUp(EmployeeSignUpModel signUpRequest)
+        {
+            var result = await repository.SigUpAsyn(signUpRequest);
+            if (result.Succeeded)
+                return Ok(result.Succeeded);
+
+            var errors = result.Errors;
+            var message = string.Join(", ", errors.Select(x => "Code " + x.Code + " Description" + x.Description));
+            return StatusCode(500, message);
+        }
+
+        [HttpPost("SignIn")]
+        public async Task<IActionResult> SignIn(EmployeeSignInModel model)
+        {
+            var result = await repository.SignInAsyn(model);
+            if (result.IsNullOrEmpty())
+                return Unauthorized();
+
+            return Ok(result);
+        }
+    }
+}
